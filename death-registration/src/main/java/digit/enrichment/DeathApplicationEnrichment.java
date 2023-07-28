e359c0caf0d6347378c8b492398faefac83a970a
ashish-egov <ashish.tiwari@egovernments.org>
2023-07-28 20:14:59 +0530

    first commit

---
+++ death-registration/src/main/java/digit/enrichment/DeathApplicationEnrichment.java
@@ -0,0 +1,98 @@
+package digit.enrichment;
+
+//import digit.service.UserService;
+//import digit.utils.IdgenUtil;
+//import digit.utils.UserUtil;
+import digit.util.IdgenUtil;
+import digit.web.models.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+@Component
+@Slf4j
+public class DeathApplicationEnrichment {
+
+    @Autowired
+    private IdgenUtil idgenUtil;
+//
+//    @Autowired
+//    private UserService userService;
+//
+//    @Autowired
+//    private UserUtil userUtils;
+
+    public void enrichDeathApplication(DeathRegistrationRequest deathRegistrationRequest) {
+        List<String> deathRegistrationIdList = idgenUtil.getIdList(deathRegistrationRequest.getRequestInfo(), deathRegistrationRequest.getDeathRegistrationApplications().get(0).getTenantId(), "dtr.registrationid", "", deathRegistrationRequest.getDeathRegistrationApplications().size());
+        Integer index = 0;
+        for(DeathRegistrationApplication application : deathRegistrationRequest.getDeathRegistrationApplications()){
+            // Enrich audit details
+            AuditDetails auditDetails = AuditDetails.builder().createdBy(deathRegistrationRequest.getRequestInfo().getUserInfo().getUuid()).createdTime(System.currentTimeMillis()).lastModifiedBy(deathRegistrationRequest.getRequestInfo().getUserInfo().getUuid()).lastModifiedTime(System.currentTimeMillis()).build();
+            application.setAuditDetails(auditDetails);
+
+            // Enrich UUID
+            application.setId(UUID.randomUUID().toString());
+
+//            application.getFather().setId(application.getId());
+//            application.getMother().setId(application.getId());
+
+            // Enrich registration Id
+            application.getAddressOfDeceased().setRegistrationId(application.getId());
+
+            // Enrich address UUID
+            application.getAddressOfDeceased().setId(UUID.randomUUID().toString());
+
+            //Enrich application number from IDgen
+            application.setRegistrationNumber(deathRegistrationIdList.get(index++));
+
+        }
+    }
+
+    public void enrichDeathApplicationUponUpdate(DeathRegistrationRequest deathRegistrationRequest) {
+//         Enrich lastModifiedTime and lastModifiedBy in case of update
+        deathRegistrationRequest.getDeathRegistrationApplications().get(0).getAuditDetails().setLastModifiedTime(System.currentTimeMillis());
+        deathRegistrationRequest.getDeathRegistrationApplications().get(0).getAuditDetails().setLastModifiedBy(deathRegistrationRequest.getRequestInfo().getUserInfo().getUuid());
+    }
+
+    public void enrichFatherApplicantOnSearch(DeathRegistrationApplication application) {
+//        UserDetailResponse fatherUserResponse = userService.searchUser(userUtils.getStateLevelTenant(application.getTenantId()),application.getFather().getId(),null);
+//        User fatherUser = fatherUserResponse.getUser().get(0);
+//        log.info(fatherUser.toString());
+//        FatherApplicant fatherApplicant = FatherApplicant.builder().aadhaarNumber(fatherUser.getAadhaarNumber())
+//                .accountLocked(fatherUser.getAccountLocked())
+//                .active(fatherUser.getActive())
+//                .altContactNumber(fatherUser.getAltContactNumber())
+//                .bloodGroup(fatherUser.getBloodGroup())
+//                .correspondenceAddress(fatherUser.getCorrespondenceAddress())
+//                .correspondenceCity(fatherUser.getCorrespondenceCity())
+//                .correspondencePincode(fatherUser.getCorrespondencePincode())
+//                .gender(fatherUser.getGender())
+//                .id(fatherUser.getUuid())
+//                .name(fatherUser.getName())
+//                .type(fatherUser.getType())
+//                .roles(fatherUser.getRoles()).build();
+//        application.setFather(fatherApplicant);
+    }
+
+    public void enrichMotherApplicantOnSearch(DeathRegistrationApplication application) {
+//        UserDetailResponse motherUserResponse = userService.searchUser(userUtils.getStateLevelTenant(application.getTenantId()),application.getFather().getId(),null);
+//        User motherUser = motherUserResponse.getUser().get(0);
+//        log.info(motherUser.toString());
+//        MotherApplicant motherApplicant = MotherApplicant.builder().aadhaarNumber(motherUser.getAadhaarNumber())
+//                .accountLocked(motherUser.getAccountLocked())
+//                .active(motherUser.getActive())
+//                .altContactNumber(motherUser.getAltContactNumber())
+//                .bloodGroup(motherUser.getBloodGroup())
+//                .correspondenceAddress(motherUser.getCorrespondenceAddress())
+//                .correspondenceCity(motherUser.getCorrespondenceCity())
+//                .correspondencePincode(motherUser.getCorrespondencePincode())
+//                .gender(motherUser.getGender())
+//                .id(motherUser.getUuid())
+//                .name(motherUser.getName())
+//                .type(motherUser.getType())
+//                .roles(motherUser.getRoles()).build();
+//        application.setMother(motherApplicant);
+    }
+}
---
+++ death-registration/src/main/java/digit/service/DeathRegistrationService.java
@@ -0,0 +1,99 @@
+package digit.service;
+
+
+import digit.enrichment.DeathApplicationEnrichment;
+import digit.kafka.Producer;
+import digit.repository.DeathRegistrationRepository;
+import digit.validators.DeathApplicationValidator;
+import digit.web.models.DeathApplicationSearchCriteria;
+import digit.web.models.DeathRegistrationApplication;
+import digit.web.models.DeathRegistrationRequest;
+//import digit.web.models.FatherApplicant;
+import lombok.extern.slf4j.Slf4j;
+import org.egov.common.contract.request.RequestInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@Slf4j
+public class DeathRegistrationService {
+
+    @Autowired
+    private DeathApplicationValidator validator;
+
+    @Autowired
+    private DeathApplicationEnrichment enrichmentUtil;
+
+//    @Autowired
+//    private UserService userService;
+//
+//    @Autowired
+//    private WorkflowService workflowService;
+
+    @Autowired
+    private DeathRegistrationRepository deathRegistrationRepository;
+
+    @Autowired
+    private Producer producer;
+
+    public List<DeathRegistrationApplication> registerBtRequest(DeathRegistrationRequest deathRegistrationRequest) {
+        // Validate applications
+        validator.validateDeathApplication(deathRegistrationRequest);
+
+        // Enrich applications
+        enrichmentUtil.enrichDeathApplication(deathRegistrationRequest);
+
+//        // Enrich/Upsert user in upon death registration
+//        userService.callUserService(deathRegistrationRequest);
+//
+//        // Initiate workflow for the new application
+//        workflowService.updateWorkflowStatus(deathRegistrationRequest);
+
+        // Push the application to the topic for persister to listen and persist
+        producer.push("save-dt-application", deathRegistrationRequest);
+
+        // Return the response back to user
+        return deathRegistrationRequest.getDeathRegistrationApplications();
+    }
+
+    public List<DeathRegistrationApplication> searchBtApplications(RequestInfo requestInfo, DeathApplicationSearchCriteria deathApplicationSearchCriteria) {
+        // Fetch applications from database according to the given search criteria
+        List<DeathRegistrationApplication> applications = deathRegistrationRepository.getApplications(deathApplicationSearchCriteria);
+
+        // If no applications are found matching the given criteria, return an empty list
+        if(CollectionUtils.isEmpty(applications))
+            return new ArrayList<>();
+
+        // Enrich mother and father of applicant objects
+//        applications.forEach(application -> {
+//            enrichmentUtil.enrichFatherApplicantOnSearch(application);
+//            enrichmentUtil.enrichMotherApplicantOnSearch(application);
+//        });
+
+        // Otherwise return the found applications
+        return applications;
+    }
+
+    public DeathRegistrationApplication updateBtApplication(DeathRegistrationRequest deathRegistrationRequest) {
+        // Validate whether the application that is being requested for update indeed exists
+//        DeathRegistrationApplication existingApplication = validator.validateApplicationExistence(deathRegistrationRequest.getDeathRegistrationApplications().get(0));
+//        existingApplication.setWorkflow(deathRegistrationRequest.getDeathRegistrationApplications().get(0).getWorkflow());
+//        log.info(existingApplication.toString());
+//        deathRegistrationRequest.setDeathRegistrationApplications(Collections.singletonList(existingApplication));
+
+        // Enrich application upon update
+        enrichmentUtil.enrichDeathApplicationUponUpdate(deathRegistrationRequest);
+
+//        workflowService.updateWorkflowStatus(deathRegistrationRequest);
+
+        // Just like create request, update request will be handled asynchronously by the persister
+        producer.push("update-dt-application", deathRegistrationRequest);
+
+        return deathRegistrationRequest.getDeathRegistrationApplications().get(0);
+    }
+}
